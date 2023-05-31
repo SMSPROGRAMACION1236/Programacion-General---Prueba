d3300b7d3a2d9cb2237bb119e7613c5ad1cf37ab
SMSPROGRAMACION1236 <santiagodavidsanabriavarela@gmail.com>
2023-05-31 16:05:59 -0400

    Leccion de udemy de las cadenas

---
+++ Cadenas.py
@@ -0,0 +1,27 @@
+## Cadenas en Python
+nombre = "Santiago {}"
+edad = 12
+nombres = """ marco
+luis
+rosa
+carlos"""
+
+
+##Trabajar con una cadena.
+#for x in nombre:
+ #   print(x)
+
+#print(nombre * 3)
+
+#print(len(nombre))
+
+## Cortar cadenas
+#print(nombre[0:8])
+
+##Cadenas y sus metodos: upper(), lower(), replace(), format()
+
+#print(nombre.upper())
+#print(nombre.lower())
+
+#print(nombre.replace("S" , "H"))
+print(nombre.format(edad))
