a5a84990fe22428986617e038e908a1f56d75801
SMSPROGRAMACION1236 <santiagodavidsanabriavarela@gmail.com>
2023-06-27 10:28:15 -0400

    Nueva leccion sobre las operaciones

---
+++ Mouredev/02_Operadores.py
@@ -0,0 +1,53 @@
+# Operadores Aritméticos.
+
+print( 3 + 4)
+print( 3 - 4)
+
+print( 3 * 4) # Multiplicacion
+print(3 / 4) # Division
+
+print( 10 % 3) # Calcula el resto
+print( 10 // 3) # Calcula una aproximacion
+print( 2 ** 3) # Calcula una potencia
+print( 2 ** 3 + 3 - 7 / 1 // 4)
+
+# Para que no salga todo junto, debes de tener espacios dentro de las comillas
+
+print("Hola " + "Python " + " Que tal? ")
+
+print("Hola " + str(4))
+
+print("Hola " * 5)
+print("Hola " * (2** 3))
+
+my_float = 2.5 * 2
+print("Hola " * int(my_float))
+
+# Operadores Comparativos.
+"""
+print(3 > 4)
+print( 3 <= 4)
+print( 3 >= 4)
+print( 3 ==  4)
+print( 3 != 4)
+print( 3 > 4 == 2)
+
+"""
+
+print("Hola" > "Python")
+print("Hola" < "Python")
+print("Hola" >= "tata")   # Ordenacion alfabetica
+print(len("Hola" ) <= len("Python"))  # Cuenta caracteres
+print("Hola"== "Python")
+print("aaaa" != "abaa")
+
+### Operadores Logicos ###
+print(3 > 4 and "Hola" > "Python") 
+print(3 > 4 or "Hola" > "Python")
+print(3 < 4 and "Hola"< "Python") 
+print(3 < 4 or ("Hola" < "Python" and 4 == 4))
+
+
+print(not(3 > 4))
+print("Hola Mundo")
+
