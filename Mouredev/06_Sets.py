c358ffe9bbf6bb83f40267a718513c59a26fea76
SMSPROGRAMACION1236 <santiagodavidsanabriavarela@gmail.com>
2023-07-08 15:07:33 -0400

    leccion sobre los sets en programacion

---
+++ Mouredev/06_Sets.py
@@ -0,0 +1,23 @@
+### Sets ###
+"""
+Tiene array, vector
+"""
+my_set = set()
+my_other_set = {}
+
+print(type(my_set))
+print(type(my_other_set)) # Inicialmente es un diccionario
+
+my_other_set = {"Santiago", "Sanabria", 15}
+print(type(my_other_set))
+
+print(len(my_other_set))  # cuenta los elementos
+
+
+my_other_set.add("SMS")
+print(my_other_set) # Un set no es una estructura ordenada
+
+
+
+
+
