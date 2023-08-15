7be26c412e9795af1e8a9aa85f8ab2d949497f4c
SMSPROGRAMACION1236 <santiagodavidsanabriavarela@gmail.com>
2023-08-15 09:53:27 -0400

    Leccion de datos booleanos y caracter

---
+++ dato_caracter/target/debug/dato_caracter.d
@@ -0,0 +1,1 @@
+C:\Users\santi\Programación\Hello\ Rust\dato_caracter\target\debug\dato_caracter.exe: C:\Users\santi\Programación\Hello\ Rust\dato_caracter\src\main.rs
---
+++ datos_boleanos/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    // Tipos de datos
+    // Boolean
+    let var1 = true; // facil
+    let var2 = false;
+    let var3: bool=true;  // manera extricta
+    let var4: bool=false;
+
+
+    println!("Es: {}", var1);
+    println!("Es: {}", var2);
+    println!("Es: {}", var3);
+    println!("Es: {}", var4);
+
+
+
+
+}
