1629afa08f1abae98322a10b02a219d42bf0620f
SMSPROGRAMACION1236 <santiagodavidsanabriavarela@gmail.com>
2023-09-10 14:53:44 -0400

    leccion sobre funciones y condicionales

---
+++ funciones/src/main.rs
@@ -0,0 +1,32 @@
+fn main() {
+    // Funciones //
+
+    
+    println!("La suma de 20 y 4 es: {}", sumar(20, 4));  // le pasamos los datos que debe sumar
+
+
+
+    // saludar("Santiago"); 
+    // saludar("Mario"); 
+    // saludar("Elias");  Descomentar
+
+    // let x = 123; // es una setencia
+    // 4 > 1; Es una expresion ya que debe devolver true o false
+    //Parametros
+    //BODY, STATEMENTS & EXPRESSION//
+    //RETUNR VALUES
+}
+
+// Debemos de especificar el tipo de dato luego de poner  un parametro
+
+// fn saludar(nombre: &str) {  // debemos de poner luego en la funcion main para que asi lo tome en cuenta
+    
+
+//     println!("Hola {}", nombre);
+// }
+
+fn sumar(num1:i32, num2:i32) ->i32  // decir que clase de datos nos tiene que retornar
+{
+
+    num1 + num2  // no lleva ;
+}
---
+++ funciones/target/debug/deps/funciones-205ff1b9dd89806a.d
@@ -0,0 +1,5 @@
+C:\Users\santi\Programación\Hello Rust\funciones\target\debug\deps\funciones-205ff1b9dd89806a.rmeta: src\main.rs
+
+C:\Users\santi\Programación\Hello Rust\funciones\target\debug\deps\funciones-205ff1b9dd89806a.d: src\main.rs
+
+src\main.rs:
