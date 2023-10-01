1328a88159c8611c62c2669e65a7e22f9bd51707
SMSPROGRAMACION1236 <santiagodavidsanabriavarela@gmail.com>
2023-10-01 15:50:22 -0300

    practica de libro de python

---
+++ Python Libros/practica1.py
@@ -0,0 +1,17 @@
+proverb1 = "Cuando el rio suena"
+proverb2 = "agua lleva"
+print(proverb1 + proverb2)
+print(proverb2[3])  # Es el indice 3 que es a
+
+lyrics = 		"""	Quizás porque mi niñez
+ Sigue jugando en tu playa
+ Y escondido tras las cañas
+ Duerme mi primer amor
+ Llevo tu luz y tu olor
+ Por dondequiera que vaya			
+
+print("rio"in proverb1)"""
+print(len(lyrics))
+
+
+print(lyrics.find("amor"))
