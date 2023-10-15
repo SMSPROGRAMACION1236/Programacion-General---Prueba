f0b68feeb9384e3b6246adabf7239e1b2d207183
SMSPROGRAMACION1236 <santiagodavidsanabriavarela@gmail.com>
2023-10-15 09:25:25 -0300

    Go MSI

--- Operadores.js
+++ Operadores.js
@@ -1,7 +1,16 @@
-numero = 10;
+let numero = 10;
 
-numero += 5; 
 
+numero %= 6
+document.write(numero)
+
+numero **3
+
+num1 = 12;
+num2 = 123;
+
+resultado = num1
+alert(resultado)
 
 document.write(numero)
 
