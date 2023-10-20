9325690936cdec7601f518dc66c59bb1d0b80fca
SMSPROGRAMACION1236 <santiagodavidsanabriavarela@gmail.com>
2023-10-20 17:36:12 -0300

    on on on on

---
+++ Concatenacion.js
@@ -0,0 +1,28 @@
+
+saludo = "Hola Pedro";
+pregunta = " Como estas";
+
+frase = saludo +  pregunta;
+
+document.write(frase);
+
+
+numx1 = " 13";
+numx2 = 121;
+numx12 = numx1.concat(numx2);
+document.write(numx12);
+
+frase1 = "Santiago Sanabria";
+frase2 = " soy " + frase1 + " y estoy caminando";
+document.write(frase2)
+
+
+// Logicos y de Comparacion
+
+let numero = 23;
+let numero2= 13;
+let text = "23";
+let text2 = "text 2";
+
+document.write( numero != numero2);
+document.write(text !== numero)  // Es mas exactamente
