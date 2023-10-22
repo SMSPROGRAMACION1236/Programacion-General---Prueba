7b7a942aef8f3902bf5dc34e29f88f7e9031d620
SMSPROGRAMACION1236 <santiagodavidsanabriavarela@gmail.com>
2023-10-22 14:07:11 -0300

    Parte dos de la autenticacion

--- Mouredev/Backend/FastAPI/routers/basic_auth_users.py
+++ Mouredev/Backend/FastAPI/routers/basic_auth_users.py
@@ -1,4 +1,4 @@
-from fastapi  import FastAPI, Depends, HTTPException
+from fastapi  import FastAPI, Depends, HTTPException, status # Tipos de errores "status", puede ser mas visual 
 from pydantic import BaseModel
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm # Se importa lo de la autenticacion  # IswordBearer, OAuth2PasswordRequestForm
 ##Autenticacion
@@ -43,13 +43,22 @@ def search_user(username:str):
   if username in users_db:
     return UserDB(users_db[username])
   
+async def current_user(token:str= Depends(oauth2)):
+  user = search_user(token)
+  if not user:
+    raise HTTPException(
+       status_code=status.HTTP_401_UNAUTHORIZED, detail="Credenciales de autenticacion Invalidas", headers={"WWW-Authenticate":"Bearer"}) # Se recomienda usar el standar
+  return user
 
+
+  
+  
 # Depends traera datos y no depende de nada
 @app.post("/login")
 async def login(form: OAuth2PasswordRequestForm = Depends()): # El que caputura los datos
   user_db= users_db.get(form.username)
   if not user_db:
-    raise HTTPException(status_code=400, detail="Incorrect usuario")
+    raise HTTPException(status.HTTP_400_BAD_REQUEST, detail="Incorrect usuario")
   
 
   user = search_user(form.username)
@@ -57,7 +66,17 @@ async def login(form: OAuth2PasswordRequestForm = Depends()): # El que caputura 
   if not form.password == user.password:
      # Comprabar si el password coinicide con la base de datos
      raise HTTPException(
-       status_code=400, detail="Incorrect password")
+       status.HTTP_400_BAD_REQUEST, detail="Incorrect password")
   
   return{"access_token": user.username, "token_type": "bearer"}
 
+
+@app.get("/users/me")
+async def me(user:User = Depends(current_user())):
+  return user
+
+
+
+# 4:40:37
+
+# Debo tomar practicar
