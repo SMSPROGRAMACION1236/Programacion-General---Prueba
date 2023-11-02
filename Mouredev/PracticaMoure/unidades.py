425049ef5375a7719005615c2f8b9b52e0568755
SMSPROGRAMACION1236 <santiagodavidsanabriavarela@gmail.com>
2023-11-02 11:03:07 -0300

    Proyecto 100% hecho por mi sobre conversor de unidades  tiene segundo, hora y minutos

--- Mouredev/PracticaMoure/unidades.py
+++ Mouredev/PracticaMoure/unidades.py
@@ -4,20 +4,41 @@
 #   segundos : int
 #   horas : 
 
-def minTohour(minutos):
+def min_To_hour(minutos):
   return minutos / 60
-  
+def seg_To_min(segundos):
+  return segundos / 60
+def hour_To_min(horas):
+  return horas * 60
 
-def prin():
+
+def change():
   
   legido = str(input("Elige:(Min, Seg, Hor): "))
-  minutos= int(input("Minutos: "))
-  segundos= int(input("Segundos: "))
-  horas= int(input("horas: "))
+  print(legido)
   if legido == "Min":
-    resultado = minTohour(minutos)
-    print(dir(legido))
+    minutos =int(input("Ingrese minutos: "))
+    print(minutos)
+    (resultado) = min_To_hour(minutos)
 
+  # print("El resultado es: ", resultado )
+  elif legido == "Seg":
+    segundos = int(input("Ingrese segundos "))
+    print(segundos)
+    resultado =  seg_To_min(segundos)
+  elif legido == "Hor":
+    horas = int(input("Ingrese horas: "))
+    print(horas)
+    resultado = hour_To_min(horas)
+  print("El resultado es: ",resultado) 
 
-  print("El resultado es:", resultado)
-prin()
+change()
+  
+  # minutos= int(input("Minutos: "))
+  # segundos= int(input("Segundos: "))
+  # horas= int(input("horas: "))
+
+#   if legido == "Min":
+#     resultado = minTohour(minutos)
+#   print("El resultado es:", resultado)
+# change()
