51b5f2c1ff20ca1fe9c5dc5ad1a85f1744336c5d
SMSPROGRAMACION1236 <santiagodavidsanabriavarela@gmail.com>
2023-12-03 09:58:44 -0300

    Correcion de insercion

--- leetcode/insertion.py
+++ leetcode/insertion.py
@@ -1,26 +1,21 @@
 
 #https://leetcode.com/problems/search-insert-position/
+# Es parecido la solucion
 
+import bisect # puedes implementar una búsqueda binaria
 
-import bisect
-
-aleatorio = int(input("Ingrese un numero: "))
-
-num = [1, 3, 5, 7, 8, 10, 13, 15, 23]
-
-
-# print(num.index(5))k\
-
+lista_numeros = [1, 3, 5, 7, 8, 10, 13, 15, 23,  25, 27, 29, 32, 34, 36, 38, 42, 53, 55, 61, 65, 72, 77, 81, 83, 89, 99, 101]
 
 try:
-  aleatorio:int
-  while aleatorio in num:
-    print("prueba con otro numero") 
-    aleatorio = int(input("Ingrese un numero: "))
-    aleatorio:int
-  
-  indice = bisect.bisect_left(num,aleatorio)
-  num.insert(indice, aleatorio)
-  print("El  numero  se puso en la lista", indice)
-except:
-  print ("No es un numero valido")
+    numero_usuario = int(input("Ingresa un número: "))
+    while numero_usuario in lista_numeros: 
+        # Con esto insistirá al usuario a ingresar un número que no está en la lista
+        print("Prueba con otro número") 
+        numero_usuario = int(input("Ingresa otro número: "))
+    
+    indice = bisect.bisect_left(lista_numeros, numero_usuario)  # Con esto te podrá dar el índice cercano más bajo posible
+    lista_numeros.insert(indice, numero_usuario) # para insertar el nuevo valor, en la lista original dada ya su índice
+    print(f"El número se puso en la lista {indice}")
+    print(f"Ahora la lista está conformada por: {lista_numeros}") 
+except ValueError: 
+    print("No es un número válido") # Si se ingresa un dato != int
