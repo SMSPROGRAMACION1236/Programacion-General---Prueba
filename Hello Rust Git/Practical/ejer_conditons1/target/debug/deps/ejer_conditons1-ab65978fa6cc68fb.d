0d639bc1ff4585b52ffe2274b3c590089bba915c
SMSPROGRAMACION1236 <santiagodavidsanabriavarela@gmail.com>
2024-08-08 16:01:31 -0400

    Preparacion para la unificacion

---
+++ Hello Rust Git/Practical/ejer_conditons1/target/debug/deps/ejer_conditons1-ab65978fa6cc68fb.d
@@ -0,0 +1,5 @@
+C:\Users\santi\Programación\Hello Rust\Practical\ejer_conditons1\target\debug\deps\ejer_conditons1-ab65978fa6cc68fb.rmeta: src\main.rs
+
+C:\Users\santi\Programación\Hello Rust\Practical\ejer_conditons1\target\debug\deps\ejer_conditons1-ab65978fa6cc68fb.d: src\main.rs
+
+src\main.rs:
---
+++ Hello Rust Git/Practical/ejer_conditons1/target/debug/deps/ejer_conditons1-ae6663b71e3f30c6.d
@@ -0,0 +1,5 @@
+C:\Users\santi\Programación\Hello Rust\Practical\ejer_conditons1\target\debug\deps\ejer_conditons1-ae6663b71e3f30c6.rmeta: src\main.rs
+
+C:\Users\santi\Programación\Hello Rust\Practical\ejer_conditons1\target\debug\deps\ejer_conditons1-ae6663b71e3f30c6.d: src\main.rs
+
+src\main.rs:
